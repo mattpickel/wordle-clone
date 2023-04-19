694c0999bbfcc07159cb83b7469788c0b5341101
mattpickel <mpickelprogramming@gmail.com>
2023-04-19 12:29:55 -0500

    Refactor App.js based on ChatGPT recommendations

--- src/components/App.js
+++ src/components/App.js
@@ -4,25 +4,22 @@ import Game from "./Game";
 import { answers } from "../data";
 
 function App() {
+  const initialGuesses = Array.from({ length: 6 }, () => []);
+  const initialTileStates = Array.from({ length: 6 }, () => Array(5).fill('grey'));
+
+  // Initialize state variables
   const [currentInput, setCurrentInput] = useState({
     currentGuess: [],
     currentGuessNumber: 1,
-    guesses: [[[]],[[]],[[]],[[]],[[]],[[]]]
+    guesses: initialGuesses
   });
 
   const [gameTileStates, setGameTileStates] = useState({
-    tileStates: [
-    ['grey', 'grey', 'grey', 'grey', 'grey'],
-    ['grey', 'grey', 'grey', 'grey', 'grey'],
-    ['grey', 'grey', 'grey', 'grey', 'grey'],
-    ['grey', 'grey', 'grey', 'grey', 'grey'],
-    ['grey', 'grey', 'grey', 'grey', 'grey'],
-    ['grey', 'grey', 'grey', 'grey', 'grey']
-    ],
+    tileStates: initialTileStates,
     round: 0
   })
 
-  var answer = answers[0].toUpperCase();
+  const answer = answers[0].toUpperCase();
 
   // Function is called when return/enter key pressed w/ 5 letters in the current guess. Returns array of green/yellow/grey strings to be delivered to game row tiles.
   function guessSubmit(guess, answer) {
@@ -52,6 +49,7 @@ function App() {
       }
     }
 
+    // Load results into Tile States Array
     setGameTileStates((prevValue) => {
       console.log('test');
       var newStates = prevValue.tileStates;
@@ -68,97 +66,71 @@ function App() {
     console.log(unguessedLetters);
   }
 
- 
+  // Higher order function to handle updates to current input
+  function updateCurrentInput(updateFn) {
+    setCurrentInput((prevValue) => {
+      const newGuess = updateFn(prevValue.currentGuess);
+      const newGuesses = prevValue.guesses.map((guess, i) =>
+        i == prevValue.currentGuessNumber - 1 ? newGuess : guess
+      );
 
-  // Key handler for handling player input. If letter, added to current guess. If backspace, delete last character from current guess. 
-  // If return/enter, submit guess if 5 letters and increase currentGuessNumber. The array of guesses is maintained accordingly. 
+      return {
+        ...prevValue,
+        currentGuess: newGuess,
+        guesses: newGuesses
+      };
+    });
+  }
+
+  // Compute 'newGuess' to be used in H.O. function
+  function handleLetterInput(value) {
+    updateCurrentInput((prevGuess) => {
+      const newGuess = prevGuess.length < 5 ? [...prevGuess, value] : [...prevGuess.slice(0, 4), value];
+      return newGuess;
+    });
+  }
+
+  function handleBackspace() {
+    updateCurrentInput((prevGuess) => {
+      const newGuess = prevGuess.slice(0, -1);
+      return newGuess;
+    })
+  }
+
+  // Submit guess on enter if valid 
+  function handleEnter() {
+    if (currentInput.currentGuess.length === 5) {
+      guessSubmit(currentInput.currentGuess, answer);
+      setCurrentInput((prevValue) => ({
+        ...prevValue,
+        currentGuess: [],
+        currentGuessNumber: prevValue.currentGuessNumber + 1
+      }));
+    }
+  }
+
+  // Call function based on keystroke
+  function handleKeyDown(event) {
+    const { keyCode } = event;
+    if (keyCode >= 65 && keyCode <= 90) {
+      handleLetterInput(event.key.toUpperCase());
+    } else if (keyCode === 8) {
+      handleBackspace();
+    } else if (keyCode === 13) {
+      handleEnter();
+    }
+  }
+
+  // Event listener on entire document to always capture user input
   useEffect(() => {
-    function handleKeyDown(event) {    
-      // Only capture keystroke if it is a letter. When captured, add it to the current guess. If it is a backspace, remove the previous letter that was guessed.    
-      if (event.keyCode >= 65 && event.keyCode <= 90) {
-        const value = event.key.toUpperCase();  
-        setCurrentInput((prevValue) => {
-          if (prevValue.currentGuess.length < 5) {         
-            var newGuess = [...prevValue.currentGuess, value];
-            var newGuesses = prevValue.guesses;
-            newGuesses[prevValue.currentGuessNumber - 1] = newGuess;   
-            return {
-              currentGuess: newGuess,
-              currentGuessNumber: prevValue.currentGuessNumber,
-              guesses: newGuesses
-            }
-          } else {
-            var newGuess = prevValue.currentGuess;
-            newGuess[4] = value;
-            var newGuesses = prevValue.guesses;
-            newGuesses[prevValue.currentGuessNumber - 1] = newGuess;
-            return {
-              currentGuess: newGuess,
-              currentGuessNumber: prevValue.currentGuessNumber,
-              guesses: newGuesses
-            }
-          }
-        });
-      // Handle Backspace
-      } else if (event.keyCode === 8) {
-        setCurrentInput((prevValue) => {
-          if (prevValue.currentGuess.length > 1) {
-            var newGuess = prevValue.currentGuess.slice(0, -1);
-            var newGuesses = prevValue.guesses;
-            newGuesses[prevValue.currentGuessNumber - 1] = newGuess;
-            return {
-              currentGuess: newGuess,
-              currentGuessNumber: prevValue.currentGuessNumber,
-              guesses: newGuesses
-            }
-          } else if (prevValue.currentGuess.length <= 1) {
-            var newGuesses = prevValue.guesses;
-            newGuesses[prevValue.currentGuessNumber - 1] = [];
-            return {
-              currentGuess: [],
-              currentKey: event.key,
-              currentGuessNumber: prevValue.currentGuessNumber,
-              guesses: newGuesses
-            }
-          }
-        })       
-        console.log("Delete last letter");
-      // Handle enter/submission
-      } else if (event.keyCode === 13) {
-        if (currentInput.currentGuess.length === 5) {
-          guessSubmit(currentInput.currentGuess, answer);
-        }
-        setCurrentInput((prevValue) => {
-          if (prevValue.currentGuess.length < 5) {
-            return prevValue;
-          } else if (prevValue.currentGuess.length === 5) {
-            var newGuesses = prevValue.guesses;
-            newGuesses[prevValue.currentGuessNumber - 1] = prevValue.currentGuess;
-            return {
-              currentGuess: [],
-              currentGuessNumber: prevValue.currentGuessNumber + 1,
-              guesses: newGuesses
-            }
-          }
-        });
-        
-      }
-    }
-
     document.addEventListener('keydown', handleKeyDown);
-
-    // Clean-up
-    return function cleanup() {
-      document.removeEventListener('keydown', handleKeyDown);
-    }
+    return () => document.removeEventListener('keydown', handleKeyDown);
   }, [currentInput]);
-
-  
 
   return (
     <div className="App">
       <Header />
-      <Game currentInput={currentInput}/>
+      <Game currentInput={currentInput} gameTileStates={gameTileStates}/>
     </div>
   );
 }
